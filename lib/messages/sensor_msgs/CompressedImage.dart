7c57800c776a256cd709bf718782e19fa4076d67
Sashiri <ali.reanc@gmail.com>
2019-12-30 17:24:43 +0100

    Add CompressedImage message format

---
+++ lib/messages/sensor_msgs/CompressedImage.dart
@@ -0,0 +1,30 @@
+import 'dart:typed_data';
+
+import 'package:ros_nodes/messages/UInt8List.dart';
+import 'package:ros_nodes/src/ros_message.dart';
+import 'package:ros_nodes/messages/String.dart';
+import 'package:ros_nodes/messages/std_msgs/Header.dart';
+
+class SensorMsgsCompressedImage extends RosMessage {
+  final StdMsgsHeader header = StdMsgsHeader();
+  final RosString _format = RosString();
+  final RosUint8List _data = RosUint8List();
+
+  String get format => _format.val;
+  set format(String value) {
+    _format.val = value;
+  }
+
+  Uint8List get data => _data.list;
+  set frame_id(Uint8List value) {
+    _data.list = value;
+  }
+
+  SensorMsgsCompressedImage()
+      : super('header data', 'std_msgs/Header',
+            '8f7a12909da2c9d3332d540a0977563f') {
+    params.add(header);
+    params.add(_format);
+    params.add(_data);
+  }
+}
