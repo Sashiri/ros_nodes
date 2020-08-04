c0f80c1f1334d147c4a0dbe20bebf48c6f034c4f
yungkk98 <yungkk87@gmail.com>
2020-08-05 06:51:00 +0900

    Support PoseWithCovariance msgs (#1)
    * Add Float64List type support
    
    * Add PoseWithCovariance message format

---
+++ lib/messages/Float64List.dart
@@ -0,0 +1,48 @@
+import 'dart:typed_data';
+import 'package:ros_nodes/src/ros_message.dart';
+
+class RosFloat64List implements BinaryConvertable {
+  Float64List list;
+  final int fixedLength;
+
+  RosFloat64List({this.fixedLength}) {
+    list = Float64List(fixedLength ?? 0);
+  }
+
+  Float64List.fromList(this.list, {this.fixedLength});
+
+  @override
+  int fromBytes(Uint8List bytes, {int offset = 0}) {
+    var size;
+    var data;
+    if (fixedLength == null) {
+      size = ByteData.view(bytes.buffer).getUint32(offset, Endian.little);
+      data = size + 4;
+    } else {
+      size = list.length;
+      data = size;
+    }
+    list = Float64List.view(bytes.buffer, offset, size);
+    return data;
+  }
+
+  @override
+  List<int> toBytes() {
+    final listLength = list.length;
+
+    Uint8List bytes;
+    if (fixedLength == null) {
+      bytes = Uint8List(4 + listLength);
+      bytes.setRange(0, 4, listLength.toBytes());
+      bytes.setRange(4, bytes.length, list);
+    } else {
+      bytes = list.buffer.asUint8List();
+    }
+    return bytes;
+  }
+
+  @override
+  String toString() {
+    return list.toString();
+  }
+}
---
+++ lib/messages/geometry_msgs/PoseWithCovariance.dart
@@ -0,0 +1,17 @@
+import 'package:ros_nodes/src/ros_message.dart';
+import 'package:ros_nodes/messages/geometry_msgs/Pose.dart';
+import 'package:ros_nodes/messages/Float64List.dart';
+
+class PoseWithCovariance extends RosMessage {
+  var pose = GeometryMsgsPose();
+  var covariance = RosFloat64List(fixedLength: 36);
+
+  PoseWithCovariance()
+      : super(
+            'pose in free space with uncertainty',
+            'geometry_msgs/PoseWithCovariance',
+            'c23e848cf1b7533a8d7c259073a97e6f') {
+    params.add(pose);
+    params.add(covariance);
+  }
+}
