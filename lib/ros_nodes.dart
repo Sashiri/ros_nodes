edf1f5b5320c4522a81bfaf09e419b5849ac0435
Sashiri <ali.reanc@gmail.com>
2021-04-26 11:51:54 +0200

    Remove initial comments from ros_nodes.dart

--- lib/ros_nodes.dart
+++ lib/ros_nodes.dart
@@ -1,6 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library ros_nodes;
 
 export 'src/ros_message.dart' show RosMessage;
@@ -9,5 +6,3 @@ export 'src/ros_subscriber.dart' show RosSubscriber;
 export 'src/ros_config.dart' show RosConfig;
 export 'src/ros_client.dart' show RosClient;
 export 'src/ros_topic.dart' show RosTopic;
-
-// TODO: Export any libraries intended for clients of this package.
