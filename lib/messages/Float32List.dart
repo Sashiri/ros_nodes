0f9cf2c1512088c2d7c98eb177d8e993320383a4
Sashiri <ali.reanc@gmail.com>
2023-01-07 00:25:27 +0100

    Add Float32 Array ROS primitive

---
+++ lib/messages/Float32List.dart
@@ -0,0 +1,18 @@
+import 'dart:typed_data';
+
+import 'NativeList.dart';
+
+class RosFloat32List extends NativeList<Float32List> {
+  RosFloat32List({int fixedLength}) : super(fixedLength: fixedLength) {
+    list = Float32List(fixedLength ?? 0);
+  }
+
+  @override
+  RosFloat32List.fromList(Float32List list, {bool isFixedLength = true})
+      : super.fromList(list, isFixedLength: isFixedLength);
+
+  @override
+  Float32List convertFromBuffer(ByteBuffer buffer) {
+    return buffer.asFloat32List();
+  }
+}
